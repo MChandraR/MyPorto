6805d4f1897bfa855a46705932a1a5f7c720ea84
MChandraR <chandramadhan04@gmail.com>
2026-07-14 22:43:11 +0700

    Add interactive and animated backround

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import AboutSection from "./components/AbountSection";
 import HeroSection from "./components/HeroSection";
 import Portfolio from "./components/Portfolio";
-
+import AIParticles from "./components/AIParticles";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen relative">
+      <AIParticles />
       <HeroSection />
       <div className="max-w-5xl mx-auto px-6 py-16 space-y-20">
         <AboutSection />
@@ -14,5 +15,3 @@ export default function Home() {
     </main>
   );
 }
-
-
